070cf9753886f7752c8ce65684f1b9dac94a34e9
HaowenLee <haowenhello@gmail.com>
2019-04-29 16:30:09 +0800

    Update gradle android migrate to androidx

--- app/src/main/java/me/haowen/soulplanet/MainActivity.java
+++ app/src/main/java/me/haowen/soulplanet/MainActivity.java
@@ -1,6 +1,6 @@
 package me.haowen.soulplanet;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
